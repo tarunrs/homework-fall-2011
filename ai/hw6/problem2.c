4f6fdf868de035b76bf28f488f68a05a4b1f7d14
Tarun R S <tarunrs@gmail.com>
2012-01-18 16:58:50 -0500

    AI last homework

--- ai/hw6/problem2.c
+++ ai/hw6/problem2.c
@@ -12,9 +12,9 @@ int total_changes = 0;
 
 struct transition {
     int from_state;
-    int to_state;
+    int to_state[3];
     int action;
-    float probability;
+    float probability[3];
 } transition_table[19];
 
 struct description {
@@ -59,9 +59,13 @@ void update_transitions(string line, int i){
         transition_table[i].action = 1;
     else
         transition_table[i].action = 2;
-
-    transition_table[i].to_state = atoi(trans.at(2).c_str());
-    transition_table[i].probability = atof(trans.at(3).c_str());
+    printf("updating transitions : %d\n", i);
+    transition_table[i].to_state[0] = atoi(trans.at(2).c_str());
+    transition_table[i].probability[0] = atof(trans.at(3).c_str());
+    transition_table[i].to_state[1] = atoi(trans.at(4).c_str());
+    transition_table[i].probability[1] = atof(trans.at(5).c_str());
+    transition_table[i].to_state[2] = atoi(trans.at(6).c_str());
+    transition_table[i].probability[2] = atof(trans.at(7).c_str());
 }
 
 
@@ -80,7 +84,7 @@ void update_desc(string line, int i){
 void read_file(void){
 
     string line;
-    ifstream transfile ("problem1.transitions");
+    ifstream transfile ("problem2.transitions");
     ifstream descfile ("problem1.desc");
     if (transfile.is_open())
     {
@@ -108,6 +112,7 @@ void read_file(void){
     descfile.close();
 
     }
+    printf("Done reading file\n");
 }
 void set_terminal(int state){
     place_descriptions[state-1].is_terminating = true;
@@ -132,7 +137,7 @@ void init(){
 void print_policy(){
     cout<<endl<<endl;
     for(int i=0; i<10; i++){
-        cout<<"State "<< policy_table[i].id+1 << ":\tUtility = " << policy_table[i].utility << "\t\tPolicy = " ;
+        cout<<"State "<< policy_table[i].id+1 << ":\tUtility = " << policy_table[i].utility << "\t\t\t\tPolicy = " ;
         if(policy_table[i].policy != -1)
             cout<< policy_strings[policy_table[i].policy]<<endl;
         else
@@ -141,27 +146,72 @@ void print_policy(){
     cout<<endl<<endl;
 }
 
-int random_action(){
-    return rand()%3;
+int random_action(double p1, double p2, double p3){
+    double rnum  = ((double) rand() / (RAND_MAX));
+    //cout << "random : "<<rnum<<endl;
+    if(rnum < p1)
+        return 0;
+    else if(rnum < p1+p2)
+        return 1;
+    else
+        return 2;
+
 }
 
 int random_state(){
     return rand()%10 +1;
 }
+float get_utility(int id){
+    float util = 0;
+    float prob1, prob2, prob3;
+    float util1, util2, util3;
+    util1 = policy_table[transition_table[id].to_state[0]-1].utility;
+    util2 = policy_table[transition_table[id].to_state[1]-1].utility;
+    util3 = policy_table[transition_table[id].to_state[2]-1].utility;
+    prob1 = transition_table[id].probability[0];
+    prob2 = transition_table[id].probability[1];
+    prob3 = transition_table[id].probability[2];
+    //printf("\n%2.2f %2.2f %2.2f p: %2.2f %2.2f %2.2f\n", util1, util2, util3, prob1, prob2, prob3);
+    util = (util1*prob1) + (util2*prob2) + (util3*prob3);
+    //printf("ID : %d Util: %f\n", id, util);
+    //printf("Utility : %2.4f", util);
+    return util;
+}
+
+float get_utility(int state_id, int move){
+    float util = 0;
+    float prob1, prob2, prob3;
+    float util1, util2, util3;
+    for(int id=0; id<18;id++){
+        if(transition_table[id].from_state == state_id && transition_table[id].action == move){
+            util1 = policy_table[transition_table[id].to_state[0]-1].utility;
+            util2 = policy_table[transition_table[id].to_state[1]-1].utility;
+            util3 = policy_table[transition_table[id].to_state[2]-1].utility;
+            prob1 = transition_table[id].probability[0];
+            prob2 = transition_table[id].probability[1];
+            prob3 = transition_table[id].probability[2];
+            //printf("\n%2.2f %2.2f %2.2f p: %2.2f %2.2f %2.2f\n", util1, util2, util3, prob1, prob2, prob3);
+            util = (util1*prob1) + (util2*prob2) + (util3*prob3);
+            //printf("ID : %d Util: %f\n", id, util);
+            //printf("Utility2 : %2.4f", util);
+            break;
+        }
+    }
+    //cout <<endl<< util<<endl;
+    return util;
+}
 
 int best_move(int state){
     int action =-1;
     float util = -10000.00;
-    //cout<< "IN best move"<<endl;
+    //cout<< "IN best move"<<state<<endl;
     for(int i=0; i<18;i++){
-
         if(transition_table[i].from_state == state){
-            //cout<< policy_table[transition_table[i].to_state-1].utility<<endl;
-            if(policy_table[transition_table[i].to_state-1].utility > util){
-                //cout<<i <<"Before: "<< util<<"\t" << action<<"\t"<< policy_table[transition_table[i].to_state-1].utility <<endl;
-                util = policy_table[transition_table[i].to_state -1].utility ;
+            if(get_utility(i) > util){
+                //cout<<i <<"Before: "<< util<<"\t" << action<<"\t"<< policy_table[transition_table[i].to_state[0]-1].utility <<endl;
+                util = get_utility(i);//policy_table[transition_table[i].to_state -1].utility ;
                 action = transition_table[i].action;//transition_table[i].to_state;
-                //cout << "After : "<< util<<"\t" << action<< "\t"<<policy_table[transition_table[i].to_state-1].utility <<endl;
+                //cout << "After : "<< util<<"\t" << action<< "\t"<<policy_table[transition_table[i].to_state[0]-1].utility <<endl;
             }
         }
     }
@@ -176,9 +226,27 @@ void update_policy(int state, int policy){
 int find_transition(int init_state, int move){
     int state;
     for(int i=0; i<18;i++){
-        if(transition_table[i].from_state == init_state && transition_table[i].action == move) return transition_table[i].to_state;
+        if(transition_table[i].from_state == init_state && transition_table[i].action == move)
+            return transition_table[i].to_state[random_action(transition_table[i].probability[0],transition_table[i].probability[1],transition_table[i].probability[2])];
+            //return transition_table[i].to_state;
     }
     return -1;
+}
+
+void print_equation(int state_id, int move){
+    float prob1, prob2, prob3;
+    float util1, util2, util3;
+    for(int id =0; id < 18; id++)
+        if(transition_table[id].from_state == state_id && transition_table[id].action == move){
+            util1 = policy_table[transition_table[id].to_state[0]-1].utility;
+            util2 = policy_table[transition_table[id].to_state[1]-1].utility;
+            util3 = policy_table[transition_table[id].to_state[2]-1].utility;
+            prob1 = transition_table[id].probability[0];
+            prob2 = transition_table[id].probability[1];
+            prob3 = transition_table[id].probability[2];
+            printf(" ( %2.2f * %2.2f + %2.2f * %2.2f + %2.2f * %2.2f ) = ", prob1, util1, prob2, util2, prob3, util3);
+            break;
+        }
 }
 
 int update_utility(int id){
@@ -193,8 +261,9 @@ int update_utility(int id){
         cout << "  Best: "<< policy_strings[bestmove] <<"  Utility("<<id<<") = ";
         update_policy(id,bestmove);
         endstate = find_transition(id,bestmove);
-        cout << util << " + (1 * " << policy_table[endstate-1].utility << ") = ";
-        util += policy_table[endstate-1].utility;
+        cout << util << " + ";
+        print_equation(id, bestmove);
+        util += get_utility(id, bestmove);
         cout<< util <<endl;
         if(policy_table[id-1].utility != util)
             total_changes++;
@@ -219,6 +288,7 @@ int main(void) {
             current_state = update_utility(current_state);
 
         }
+        //getchar();
         num_random_restarts++;
     }while(total_changes != 0 || num_random_restarts < 100);
     print_policy();
