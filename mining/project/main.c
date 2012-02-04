777e5b9eeb675c46293a100c01f031f6ee9cb662
Tarun R S <tarunrs@gmail.com>
2012-02-03 19:32:16 -0500

    genertating arff files for weka.

--- mining/project/main.c
+++ mining/project/main.c
@@ -12,6 +12,8 @@
 #include <map>
 #include <locale>
 #include <sys/time.h>
+#include <time.h>
+
 #include <string.h>
 #include <stdlib.h>      /* for malloc, free */
 #include <ctype.h>       /* for isupper, islower, tolower */
@@ -37,16 +39,18 @@ struct feature_vector {
 
 struct config {
     bool date, topics, places, people, orgs, exchanges, companies, unknown, title, dateline, body, stopwords, stemwords, limit;
-    bool gen_fv_set, gen_fv_count, gen_fv_count_sparse, gen_fd_file, gen_fd_unique_file, gen_wf_file, gen_wf_unique_file, gen_dimension_file, gen_fd_with_strings, gen_fv_title;
+    bool gen_fv_set, gen_fv_count, gen_fv_count_sparse, gen_fd_file, gen_fd_unique_file, gen_wf_file, gen_wf_unique_file, gen_dimension_file, gen_fd_with_strings, gen_fv_title, gen_random;
     int threshold_min, threshold_max;
     int dim_limit;
     int num_files;
+    int num_arff_data;
 };
 
 map<string, int> words;
 map<string, int> words_unique;
 vector< map< string, int > > candidate_sets;
 set <string> stop_words;
+set <string> class_labels;
 vector<article> articles;
 vector<feature_vector> features ;
 multimap<int, int> frequency_distribution;
@@ -64,6 +68,8 @@ void load_conf(){
         ff >> config_value;
         if(config_key == "num-files")
             conf.num_files = atoi(config_value.c_str());
+        else if(config_key == "num-arff-data")
+            conf.num_arff_data = atoi(config_value.c_str());
         else if(config_key == "include-date")
             conf.date = (config_value == "yes" ? true : false);
         else if(config_key == "include-topics")
@@ -98,6 +104,8 @@ void load_conf(){
             conf.threshold_max = atoi(config_value.c_str());
         else if(config_key == "gen-fv-set")
             conf.gen_fv_set = (config_value == "yes" ? true : false);
+        else if(config_key == "gen-random")
+            conf.gen_random = (config_value == "yes" ? true : false);
         else if(config_key == "gen-fv-count")
             conf.gen_fv_count = (config_value == "yes" ? true : false);
         else if(config_key == "gen-fv-count-sparse")
@@ -212,12 +220,19 @@ string delete_tags(string tag, string text){
     if(index != string::npos)
     {
         end_index = text.find(">", index);
-        text.erase(index, end_index - index+1);
+        if(end_index != string::npos)
+            {text.erase(index, end_index - index + 1);}// cout << "erased" <<endl;}
+        //cout << text <<endl;
     }
 
     index = text.find("</" + tag + ">");
     if(index != string::npos)
-        text.erase(index, tag.length()+3);
+    {
+        //cout << "text :" << text << endl;
+        text.erase(index);//, tag.length()+3);
+
+    }
+
     //cout << text <<endl;
     return text;
 }
@@ -361,6 +376,13 @@ string clean_up(string data){
             data.erase(data.length()-1);
         else if(data.rfind("'s") == data.length()-2)
             data.erase(data.length()-2);
+        else if(data.rfind("'t") == data.length()-2)
+            data.erase(data.length()-2);
+
+        else if(data.rfind("'r") == data.length()-2)
+            data.erase(data.length()-2);
+        else if(data.rfind("'") != string::npos)
+            data.erase(data.rfind("'"));
         else if(data.find("\"") == 0)
             data.erase(0, 1);
         else if(data.find("(") == 0)
@@ -448,6 +470,7 @@ void parse_and_enter_into_set(article art){
         while(ss >> temp){
             temp = clean_up(temp);
             if(conf.stemwords) temp = stem_word(temp);
+            //temp = clean_up(temp);
             if(temp.length() > 1)  {
                 if(words.find(temp) != words.end()){
                     words.find(temp)->second++;
@@ -472,6 +495,85 @@ void parse_and_enter_into_set(article art){
     }
     candidate_sets.push_back(candidate_set);
 }
+
+void generate_arff_file(){
+    ofstream ff( "results/features.arff", ios::out );
+    ofstream ff1( "results/test.arff", ios::out );
+    ofstream ff2( "results/test.indexes", ios::out );
+    stringstream ss;
+    string temp;
+    feature_vector f_vec;
+    unsigned int j  = 0;
+    ff << "@RELATION news" << endl;
+    ff1 << "@RELATION news" << endl;
+    set<string>::const_iterator pos;
+    int index = 1;
+    cout << "Writing Dimensions to file" << endl;
+    for(pos = dimensions.begin(); pos != dimensions.end(); ++pos)
+        {
+            ff << "@ATTRIBUTE '"<< *pos << "' NUMERIC" << endl;
+            ff1 << "@ATTRIBUTE '"<< *pos << "' NUMERIC" << endl;
+        }
+    ff << "@ATTRIBUTE class {";
+    ff1 << "@ATTRIBUTE class {";
+    for(pos = class_labels.begin(); pos != class_labels.end(); ++pos)
+        {
+        ff <<*pos << "," ;
+        ff1 <<*pos << "," ;
+        }
+        ff << "}" << endl << "@DATA" << endl;// << "{";
+        ff1 << "}" << endl << "@DATA" << endl;// << "{";
+    cout << "Number of records generating: " << conf.num_arff_data << endl;
+    //for(unsigned int i =0 ; i < conf.num_arff_data; ){ //uncomment is random sampling
+    for(unsigned int i =0 ; i < features.size(); i++){
+        //int ran = rand() %  features.size(); //uncomment is random sampling
+
+        //f_vec  = features.at(ran); //uncomment is random sampling
+        f_vec  = features.at(i);
+        if(f_vec.class_labels.size() == 0) {
+            ff1 << "{" ;
+            for(j = 0; j < f_vec.counts.size(); j++)
+                if(f_vec.counts.at(j) != 0)
+                    ss <<  j << " " << f_vec.counts.at(j) << ", ";
+            //ff1 <<  j << " " << f_vec.counts.at(j) ;
+            // only including the first class-label.
+            //cout << "hmm" << endl;
+
+            //ff1 << j << " \""<< *pos <<
+            temp = ss.str();
+            ss.str("");
+            if(temp.length() > 2)
+                temp.erase(temp.length()-2);
+            ff1 << temp << "}" << endl;
+            ff2 << i + 1 << endl;
+            //ff2 << ran+1 << endl;
+            //ff1 <<  endl;
+        }
+        //if(f_vec.)
+        else{
+            //for(pos = f_vec.class_labels.begin(); pos != f_vec.class_labels.end(); ++pos){
+            cout <<i<< " ";
+            pos = f_vec.class_labels.begin();
+            ff << "{" ;
+            for(j = 0; j < f_vec.counts.size(); j++)
+                if(f_vec.counts.at(j) != 0) ff <<  j << " " << f_vec.counts.at(j) << ", ";
+            // only including the first class-label.
+            //cout << "hmm" << endl;
+            int random_num = rand() % f_vec.class_labels.size();
+            int temp_ind = 0;
+            for(pos = f_vec.class_labels.begin(); pos != f_vec.class_labels.end() && temp_ind < random_num; ++pos, temp_ind++) {};
+
+            ff << j << " \""<< *pos << "\"}" << endl;
+            //i++; // uncomment of random
+            //}
+        }
+    }
+    ff.close();
+    ff1.close();
+
+    ff2.close();
+}
+
 
 void generate_dimension_set(){
     cout << "Generating dimensions" << endl;
@@ -501,6 +603,7 @@ void generate_feature_vectors(){
         for(unsigned int j = 0; j < art.topics.size(); j++)
             {
             vec.class_labels.insert(art.topics.at(j));
+            class_labels.insert(art.topics.at(j));
             }
         for(pos = candidate_set.begin(); pos != candidate_set.end(); ++pos){
             // here we need to truncate based on a set of valid words.
@@ -524,7 +627,7 @@ void generate_feature_vectors(){
 }
 
 void dump_dimensions_to_file(){
-    ofstream ff( "dimensions", ios::out );
+    ofstream ff( "results/dimensions", ios::out );
     set<string>::const_iterator pos;
     int index = 1;
     cout << "Writing Dimensions to file" << endl;
@@ -542,13 +645,13 @@ void dump_feature_vectors(){
     ofstream ff3;
     feature_vector f_vec;
     if(conf.gen_fv_set)
-        ff.open ( "fv-word-set");
+        ff.open ( "results/fv-word-set");
     if(conf.gen_fv_count)
-        ff1.open ( "fv-word-count");
+        ff1.open ( "results/fv-word-count");
     if(conf.gen_fv_count_sparse)
-        ff2.open ( "fv-word-count-sparse");
+        ff2.open ( "results/fv-word-count-sparse");
     if(conf.gen_fv_title)
-        ff3.open ( "fv-titular-words");
+        ff3.open ( "results/fv-titular-words");
     if ( (conf.gen_fv_set && !ff.is_open())
         || (conf.gen_fv_count && !ff1.is_open() )
         || (conf.gen_fv_count_sparse && !ff2.is_open() )
@@ -573,10 +676,12 @@ void dump_feature_vectors(){
             ff3 << i+1 << " { ";
         // write class labels
         // if config is to generate feature vector set of strings
+
         if(conf.gen_fv_set){
             for(pos1 = f_vec.class_labels.begin(); pos1 != f_vec.class_labels.end(); ++pos1 )
                 ff << *pos1 << " ";
             ff << "}, { ";
+            //ff << "}, { ";
             // write feature vectors
             ff << f_vec.words.size();
             ff << " }, { ";
@@ -586,18 +691,36 @@ void dump_feature_vectors(){
         }
 
         if(conf.gen_fv_count){
+            for(pos1 = f_vec.class_labels.begin(); pos1 != f_vec.class_labels.end(); ++pos1 )
+                ff1 << *pos1 << " ";
+            ff1 << "}, { ";
+            //ff << "}, { ";
+            // write feature vectors
+
             for(unsigned int j = 0; j < f_vec.counts.size(); j++)
                 ff1 << f_vec.counts.at(j) << " ";
             ff1 << "}" << endl;
         }
 
         if(conf.gen_fv_count_sparse){
+            for(pos1 = f_vec.class_labels.begin(); pos1 != f_vec.class_labels.end(); ++pos1 )
+                ff2 << *pos1 << " ";
+            ff2 << "}, { ";
+            ff2 << f_vec.words.size();
+            ff2 << " }, { ";
+
             for(unsigned int j = 0; j < f_vec.counts.size(); j++)
                 if(f_vec.counts.at(j) != 0) ff2 << j+1 << " (" << f_vec.counts.at(j) << ") ";
             ff2 << "}" << endl;
         }
 
         if(conf.gen_fv_title){
+            for(pos1 = f_vec.class_labels.begin(); pos1 != f_vec.class_labels.end(); ++pos1 )
+                ff3 << *pos1 << " ";
+            ff3 << "}, { ";
+            ff3 << f_vec.titular_words.size();
+            ff3 << " }, { ";
+
             for(pos1 = f_vec.titular_words.begin(); pos1 != f_vec.titular_words.end(); ++pos1 )
                 ff3 << *pos1 << " ";
             ff3 << "}" <<endl;
@@ -627,7 +750,7 @@ void dump_word_frequencies_to_file(){
     map<string, int>::const_iterator pos;
 
     if(conf.gen_wf_file){
-        ofstream ff( "word_frequencies", ios::out );
+        ofstream ff( "results/word_frequencies", ios::out );
         if ( !ff.is_open() )
         return;
         cout << "Writing word frequencies to file" << endl;
@@ -638,7 +761,7 @@ void dump_word_frequencies_to_file(){
 
     }
     if(conf.gen_wf_unique_file){
-        ofstream ff1( "unique_word_frequencies", ios::out );
+        ofstream ff1( "results/unique_word_frequencies", ios::out );
         if ( !ff1.is_open() )
         return;
         for(pos = words_unique.begin(); pos != words_unique.end(); ++pos)
@@ -654,12 +777,15 @@ void dump_frequency_distribution_to_file(){
 
     ofstream ff, ff1;
     if(conf.gen_fd_file)
-        ff.open( "frequency_distribution");
+        ff.open( "results/frequency_distribution");
     if(conf.gen_fd_unique_file)
-        ff1.open( "unique_frequency_distribution");
+        ff1.open( "results/unique_frequency_distribution");
 
-    if ( (conf.gen_fd_file && !ff.is_open() )|| ( conf.gen_fd_unique_file && !ff1.is_open() ))
+    if ( (conf.gen_fd_file && !ff.is_open() )|| ( conf.gen_fd_unique_file && !ff1.is_open() )){
+        cout << "Unable to write to file" << endl;
         return;
+    }
+
 
     cout << "Writing frequency distribution to file" << endl;
 
@@ -674,7 +800,7 @@ void dump_frequency_distribution_to_file(){
     }
 
 
-    if(conf.gen_fd_file){
+    if(conf.gen_fd_unique_file ){
         pos2 = unique_frequency_distribution.end();
         pos2--;
         for(; pos2 != unique_frequency_distribution.begin(); pos2--){
@@ -688,18 +814,18 @@ void dump_frequency_distribution_to_file(){
 }
 
 void dump_frequency_distribution_to_file_with_strings(){
-        map<string, int>::const_iterator pos;
+    map<string, int>::const_iterator pos;
     multimap<int, int>::const_iterator pos1;
     multimap<int, int>::const_iterator pos2;
 
     ofstream ff, ff1;
     if(conf.gen_fd_file)
-        ff.open( "frequency_distribution");
+        ff.open( "results/frequency_distribution");
     if(conf.gen_fd_unique_file)
-        ff1.open( "unique_frequency_distribution");
+        ff1.open( "results/unique_frequency_distribution");
 
     if ( (conf.gen_fd_file && !ff.is_open() )|| ( conf.gen_fd_unique_file && !ff1.is_open() ))
-        return;
+        { cout<< "Could not open file" <<endl; return;}
 
     cout << "Writing frequency distribution to file" << endl;
 
@@ -784,9 +910,9 @@ void load_file(){
 
     for(int i= 0; i < conf.num_files; i++){
     //for(int i= 0; i < 2; i++){
-        char filename[14];
-        if(i <10) sprintf(filename, "reut2-00%d.sgm", i);
-        else sprintf(filename, "reut2-0%d.sgm", i);
+        char filename[24];
+        if(i <10) sprintf(filename, "dataset/reut2-00%d.sgm", i);
+        else sprintf(filename, "dataset/reut2-0%d.sgm", i);
         ifstream ff( filename, ios::in );
         if ( !ff.is_open() )
              {cout << i ; exit(-1);}
@@ -850,6 +976,7 @@ void load_file(){
 int main()
 {
     double clkbegin, clkend, t;
+    srand (time(NULL));
     load_stop_words();
     load_conf();
     print_conf();
@@ -894,7 +1021,7 @@ int main()
 
     if(conf.gen_dimension_file)
         dump_dimensions_to_file();
-
+    generate_arff_file();
     clkend = rtclock();
     t = clkend-clkbegin;
     cout << "Time: " << t << "sec " <<endl;
